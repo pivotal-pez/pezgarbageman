4760b161bb4f701156587070f5a957805f17c933
John Calabrese <jcalabrese@pivotal.io>
2015-05-31 00:13:23 -0400

    [#95736904] poc grab uaa user list is working
    we now have everything we need to actually start coding
    against the acceptance criteria.

--- cli/pezgc/main.go
+++ cli/pezgc/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
 
+	cf "github.com/pivotalservices/pezdispenser/cloudfoundryclient"
+	"github.com/xchapter7x/cloudcontroller-client"
+)
+
+type logger struct{}
+
+func (s *logger) Println(l ...interface{}) {
+	if os.Getenv("LOG_DEBUG") == "true" {
+		fmt.Println(l...)
+	}
+}
+
+type heritage struct {
+	*ccclient.Client
+	ccTarget string
+}
+
+func (s *heritage) CCTarget() string {
+	return s.ccTarget
+}
+
+func main() {
+	baseURI := os.Getenv("CF_DOMAIN")
+	user := os.Getenv("CF_USER")
+	pass := os.Getenv("CF_PASS")
+	loginURI := fmt.Sprintf("https://%s.%s", "login", baseURI)
+	apiURI := fmt.Sprintf("https://%s.%s", "api", baseURI)
+	heritageClient := &heritage{
+		Client:   ccclient.New(loginURI, user, pass, new(http.Client)),
+		ccTarget: apiURI,
+	}
+	heritageClient.Login()
+	cfclient := cf.NewCloudFoundryClient(heritageClient, new(logger))
+	cfclient.QueryAPIInfo()
+	u, _ := cfclient.QueryUsers(1, 1, "id", "")
+	users, _ := cfclient.QueryUsers(1, u.TotalResults, "userName,meta", url.QueryEscape("origin eq 'uaa'"))
+
+	for _, v := range users.Resources {
+		fmt.Printf("Created: %s Modified: %s User: %s\n", v.Meta["created"], v.Meta["lastModified"], v.UserName)
+	}
+	fmt.Println("UAA Users Found: ", users.TotalResults)
 }
